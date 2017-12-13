345de42dd17ce4a543509336a39e94689df9d1d5
qianyigit <2442051156@qq.com>
2017-12-13 19:48:17 +0800

    添加验证码

--- src/com/main/java/com/yr/dao/LinkMysql.java
+++ src/com/main/java/com/yr/dao/LinkMysql.java
@@ -1,47 +1,47 @@
 package com.yr.dao;
 
-
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.util.Properties;
 
 public class LinkMysql {
-	//���ݿ�����
-		public Connection getCon(){
-			Connection conn=null;
-			try {
-				Class.forName("com.mysql.jdbc.Driver");
-				conn=(Connection) DriverManager.getConnection("jdbc:mysql://192.168.1.253:3306/work?useUnicode=true&characterEncoding=UTF-8","root","root");	
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			return conn;
-		}
-		
-		/**
-		 * //�ر���
-		 * @param conn
-		 */
-		public static void close(Connection conn,PreparedStatement prepar,ResultSet resu,Statement stat) {
+    private static String ip = null;
+    private static String driver = null;
+    private static String username = null;
+    private static String password = null;
+    private static String data = null;
 
-			try {
-				if(resu!=null){
-					resu.close();
-				}
-				if(prepar!=null){
-					prepar.close();
-				}
-				if(stat!=null){
-					stat.close();
-				}
-				if(conn!=null){
-				 conn.close();
-				}
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
+    public static Connection getCon() {
+        getConnection();
+        Connection con = null;
+        try {
+            Class.forName(driver);
+            con = (Connection) DriverManager.getConnection(
+                    "jdbc:mysql://" + ip + "/" + data + "?useUnicode=true&characterEncoding=UTF-8", username, password);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return con;
+    }
 
-		}
+    public static void getConnection() {
+        try {
+            // 加载属性文件，读取数据库连接配置信息
+            Properties pro = new Properties();
+            try {
+                pro.load(LinkMysql.class.getResourceAsStream("/jdbc.properties"));
+            } catch (IOException e) {
+                System.out.println("未找到配置文件！！！");
+            }
+
+            ip = pro.getProperty("ip");
+            driver = pro.getProperty("driver");
+            username = pro.getProperty("username");
+            password = pro.getProperty("password");
+            data = pro.getProperty("data");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
