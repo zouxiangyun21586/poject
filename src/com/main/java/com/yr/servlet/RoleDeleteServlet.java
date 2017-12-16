5803add86ef2b0d92d88e36ebed1c137c84855e2
tangzihaogit <2060735389@qq.com>
2017-12-16 14:54:00 +0800

    sadf

---
+++ src/com/main/java/com/yr/servlet/RoleDeleteServlet.java
@@ -0,0 +1,60 @@
+package com.yr.servlet;
+
+import java.io.IOException;
+import java.sql.ResultSet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.PreparedStatement;
+import com.yr.dao.LinkMysql;
+
+public class RoleDeleteServlet extends HttpServlet {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		try {
+			Connection conn = (Connection) LinkMysql.getCon();
+			String id = req.getParameter("id");
+			String sql = "select r.id from role r,account a, account_role ar where r.id = ar.role_id and a.id = ar.account_id and a.id=? ";
+			PreparedStatement ps = (PreparedStatement) conn.prepareStatement(sql);
+			ps.setString(1, id);
+			ps.executeQuery();
+			ResultSet rs = ps.getResultSet();
+			rs.next();
+			int count = rs.getInt(1);
+			if (count == 1) {
+				resp.getWriter().write("0");
+			} else if (count == 2) {
+				resp.getWriter().write("1");
+			} else if (count <= 7 && count > 2) {
+				resp.getWriter().write("2");
+			} else if (count == 8) {
+				resp.getWriter().write("3");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		try {
+			String id = req.getParameter("id");
+			Connection conn = (Connection) LinkMysql.getCon();
+			String sql = "delete from account where id = ?";
+			PreparedStatement ps = (PreparedStatement) conn.prepareStatement(sql);
+			ps.setString(1, id);
+			ps.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ src/com/main/java/com/yr/servlet/RoleQueryServlet.java
@@ -0,0 +1,58 @@
+package com.yr.servlet;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.yr.dao.LinkMysql;
+import com.yr.pojo.Role;
+import com.yr.pojo.RoleTzh;
+import com.yr.util.JsonUtils;
+
+public class RoleQueryServlet extends HttpServlet {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+	}
+	/* 作者：唐子壕
+	 * @时间：2017年12月15日下午8:29:40
+	 */
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setCharacterEncoding("UTF-8");
+		try {
+			Connection conn = LinkMysql.getCon();
+			String sql = "select a.id,a.name,a.account,r.roleName,ar.state from role r,account a, account_role ar where r.id = ar.role_id and a.id = ar.account_id";
+			PreparedStatement ps = (PreparedStatement) conn.prepareStatement(sql);
+			ps.executeQuery();
+			ResultSet rs = ps.getResultSet();
+			List<RoleTzh> list = new ArrayList<>();
+			while (rs.next()){
+				RoleTzh role = new RoleTzh();
+				role.setId(rs.getInt(1));
+				role.setAccountName(rs.getString(2));
+				role.setAccount(rs.getString(3));
+				role.setRoleName(rs.getString(4));
+				role.setState(rs.getInt(5));
+				list.add(role);
+			}
+			String strjson = JsonUtils.beanListToJson(list);
+			resp.getWriter().write(strjson);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
