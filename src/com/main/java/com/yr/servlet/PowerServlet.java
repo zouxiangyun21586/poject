b000e6a028c2014eee46b6823ce3c411d2e39f1e
zhouyehaogit2498 <2498230018@qq.com>
2017-12-21 22:20:47 +0800

    修改了jilian方法的调用位置

--- src/com/main/java/com/yr/servlet/PowerServlet.java
+++ src/com/main/java/com/yr/servlet/PowerServlet.java
@@ -1,19 +1,13 @@
 package com.yr.servlet;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.util.ArrayList;
-import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.yr.dao.LinkMysql;
-import com.yr.pojo.Mune;
+import com.yr.dao.PowerDao;
 import com.yr.util.JsonUtils;
 
 public class PowerServlet extends HttpServlet {
@@ -24,31 +18,10 @@ public class PowerServlet extends HttpServlet {
         resp.setContentType("text/json");
         resp.setCharacterEncoding("utf-8");
         String id = req.getParameter("id");
-        String json = JsonUtils.beanListToJson(jilian(Integer.valueOf(id)));
+        String json = JsonUtils.beanListToJson(PowerDao.jilian(Integer.valueOf(id)));
         resp.getWriter().write(json);
     }
 
-    public static List<Mune> jilian(Integer id) {
-        try {
-            String sql = "select * from menu where menu_id=?";
-            Connection con = LinkMysql.getCon();
-            PreparedStatement ps = con.prepareStatement(sql);
-            ps.setInt(1, id);
-            ResultSet rs = ps.executeQuery();
-            List<Mune> list = new ArrayList<>();
-            while (rs.next()) {
-                Mune j = new Mune();
-                j.setId(rs.getInt(1));
-                j.setMuneId(rs.getInt(2));
-                j.setFatherName(rs.getString(3));
-                j.setUrl(rs.getString(4));
-                list.add(j);
-            }
-            return list;
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
+    
 
 }
