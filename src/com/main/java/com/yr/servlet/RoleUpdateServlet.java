a91a94995426706006b88ebe5439d59b6a08a151
zhouyehaogit2498 <2498230018@qq.com>
2018-01-17 14:46:34 +0800

    解决报错

--- src/com/main/java/com/yr/servlet/RoleUpdateServlet.java
+++ src/com/main/java/com/yr/servlet/RoleUpdateServlet.java
@@ -10,8 +10,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.tomcat.util.digester.Rules;
-
 import com.mysql.jdbc.Connection;
 import com.mysql.jdbc.PreparedStatement;
 import com.yr.dao.LinkMysql;
