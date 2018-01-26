584f1ad1b8b7bb9ff8e6e72b24a91d9b5c4c8a7f
zhouyehaogit2498 <2498230018@qq.com>
2018-01-26 08:20:53 +0800

    代码

--- src/com/main/java/com/yr/dao/BuyersDao.java
+++ src/com/main/java/com/yr/dao/BuyersDao.java
@@ -1,9 +1,88 @@
 package com.yr.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.yr.pojo.Seller;
+
 /**
  * 买家数据持久层
  * @author 周业好
  * 2018年1月23日 上午10:55:25
  */
 public class BuyersDao {
+	private static final int NUB_0 = 0;
+    private static final int NUB_1 = 1;
+    private static final int NUB_2 = 2;
+    private static final int NUB_3 = 3;
+    private static final int NUB_4 = 4;
+    private static final int NUB_5 = 5;
+    private static final int NUB_6 = 6;
+    private static final int NUB_7 = 7;
+    private static final int NUB_8 = 8;
+    private static final int NUB_9 = 9;
+    private static final int NUB_10 = 10;
+    private static final int NUB_11 = 11;
+    private static final int NUB_12 = 12;
+    private static final int NUB_13 = 13;
+    private static final int NUB_14 = 14;
+    private static final int NUB_15 = 15;
+    private static final int NUB_16 = 16;
+    private static final int NUB_17 = 17;
+    private static final int NUB_18 = 18;
+    private static final int NUB_19 = 19;
 
+    private static Integer number = null;
+    
+    /**
+     * 查询卖家保存商品的所有信息
+     * @param id        卖家商品表ID
+     * @return          卖家商品信息
+     * List<Seller>
+     * 2018年1月24日下午8:21:50
+     */
+    public static List<Seller> queryGoods(Integer id) {
+        Connection conn = LinkMysql.getCon();
+        List<Seller> list = new ArrayList<>();
+        try {
+            String sql = "select mt.type,m.`name`,m.`describe`,spt.origin,spt.netContent,spt.packingMethod,spt.brand,mth.`month`,spt.qGP,spt.storageMethod,m.money,m.number,m.upFrameTime,rs.time,rs.downtime,rs.id,m.id,spt.id,m.nameTypeID from seller rs,merchandise m,merchandise_type mt,specification_table spt,month_table mth where rs.wares_id=m.id and m.nameTypeID=mt.id and m.specificationID=spt.id and mth.id=spt.qGP and rs.id=?;";
+            PreparedStatement ps = (PreparedStatement) conn.prepareStatement(sql);
+            ps.setInt(NUB_1, id);
+            ps.executeQuery();
+            ResultSet rs = ps.getResultSet();
+            while (rs.next()) {
+                Seller goods = new Seller();
+                goods.setNameType(rs.getString(NUB_1));
+                goods.setName(rs.getString(NUB_2));
+                goods.setDescribe(rs.getString(NUB_3));
+                goods.setOrigin(rs.getString(NUB_4));
+                goods.setNetContent(rs.getString(NUB_5));
+                goods.setPackingMethod(rs.getString(NUB_6));
+                goods.setBrand(rs.getString(NUB_7));
+                goods.setMonth(rs.getString(NUB_8));
+                goods.setqGP(rs.getInt(NUB_9));
+                goods.setStorageMethod(rs.getString(NUB_10));
+                goods.setMoney(rs.getString(NUB_11));
+                goods.setNumber(rs.getInt(NUB_12));
+                goods.setUpFrameTime(rs.getString(NUB_13));
+                goods.setTime(rs.getString(NUB_14));
+                goods.setDowntime(rs.getString(NUB_15));
+                goods.setId(rs.getInt(NUB_16));
+                goods.setWares_id(rs.getInt(NUB_17));
+                goods.setSpeciID(rs.getInt(NUB_18));
+                goods.setNameTypeID(rs.getInt(NUB_19));
+                list.add(goods);
+            }
+            rs.close();
+            ps.close();
+            return list;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    
 }
--- src/com/main/java/com/yr/servlet/BuyersServlet.java
+++ src/com/main/java/com/yr/servlet/BuyersServlet.java
@@ -1,23 +1,112 @@
 package com.yr.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.yr.dao.BuyersDao;
+import com.yr.dao.LinkMysql;
+import com.yr.dao.SellerDao;
+import com.yr.pojo.Seller;
+import com.yr.util.ConnectTime;
+import com.yr.util.JsonUtils;
+import com.yr.util.PageService;
+
+import net.sf.json.JSONObject;
 /**
  * 买家业务逻辑
  * @author 周业好
  * 2018年1月23日 上午10:55:09
  */
 public class BuyersServlet extends HttpServlet{
-	private static final long serialVersionUID = 1L;
-	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-	}
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-	}
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 卖家表
+     * i=1 o=1 为    查看商品详细信息
+     * i=1 o=2 为    修改页面
+     * i=2 为    删除保存的商品信息
+     * i=3 为    撤销正在审核的商品
+     * i=4 为    下架商品
+     * i=5 为    修改商品信息
+     * i=6 为    添加商品
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+     *      response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        response.setContentType("text/json");
+        response.setCharacterEncoding("utf-8"); // 转码 防止中文乱码
+        int seller_id = (int)request.getSession().getAttribute("userID");
+        String o = request.getParameter("o");
+        String i = request.getParameter("i");
+        try {
+            if ("1".equals(i)) { // 查看或修改商品详细信息
+            	int id = Integer.valueOf(request.getParameter("id"));
+                List<Seller> list = BuyersDao.queryGoods(id);
+                if ("1".equals(o)) { // 进入查看页面
+                    request.setAttribute("list", list);
+                    request.getRequestDispatcher("seller/detail.jsp").forward(request, response);
+                } else if ("2".equals(o)) { // 进入修改页面
+                    request.setAttribute("list", list);
+                    request.getRequestDispatcher("seller/update.jsp").forward(request, response);
+                }
+            }else if("2".equals(i)){
+            	
+            }
+        } catch (Exception e) {
+            response.getWriter().write("1");
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 查询卖家表所有数据
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+     *      response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/json");
+        response.setCharacterEncoding("utf-8");
+        //页面显示值
+        PrintWriter out = response.getWriter();
+        String type= request.getParameter("type");
+        String select= request.getParameter("select");//搜索功能中输入框的值(账号)
+        String pageNow = request.getParameter("pageNow");//获得页面传过来的当前页
+        if (null != type && type.equals("list")) {
+            String sel = request.getParameter("select");
+            if (null == pageNow || "".equals(pageNow)) {
+                pageNow = "1";
+            }
+         // 查询并分页
+            List<Seller> list = SellerDao.selectGoods(select,Integer.valueOf(pageNow),sel);
+         // 获得总页数
+            int pageCount=SellerDao.getPageCount();
+         // 显示当前页
+            String pageCode = new PageService().getPageCode(Integer.parseInt(pageNow), pageCount);
+            Map<String, Object> map = new HashMap<>();
+            map.put("list", list);
+            map.put("pageCount", pageCount + "");
+            map.put("pageNow", pageNow);
+            map.put("pageCode", pageCode);
+            String jsonObjectStr = JSONObject.fromObject(map).toString();
+            out.write(jsonObjectStr);
+            out.flush();
+            out.close();
+        }
+    }
 }
